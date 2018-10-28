922490b53aac656def315c8c682e888fd3be3047
jiachenyu <jiachenyu@bytendance.com>
2018-10-28 13:14:24 +0800

    handlerThread activity

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # AndroidMultiThread
+
+### HandlerThread  
+使用handler来进行UIThread 和 子线程直接通信
+相对于直接用handler，HandlerThread简化了使用过程，把Looper.prepare(), Looper.loop()等过程包含在内，不需要用户每次重复写此几行代码
--- app/src/main/java/com/example/jiacy/multithreadtest/MainActivity.java
+++ app/src/main/java/com/example/jiacy/multithreadtest/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         handlerbutton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, AsyncTaskActivity.class);
+                Intent intent = new Intent(MainActivity.this, ThreadHandlerActivity.class);
                 startActivity(intent);
             }
         });
